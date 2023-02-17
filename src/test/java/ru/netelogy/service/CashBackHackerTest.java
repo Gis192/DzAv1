f2d644d2da944f1e1b2f4d3867f853244b91cdf4
Ivan Golikov <gis92@rambler.ru>
2023-02-17 22:33:57 +0300

    JUnit4

---
+++ src/test/java/ru/netelogy/service/CashBackHackerTest.java
@@ -0,0 +1,41 @@
+package ru.netelogy.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public  class CashBackHackerTest {
+
+    @Test
+   public void shouldReturn100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldReturn1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldReturn0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+}
