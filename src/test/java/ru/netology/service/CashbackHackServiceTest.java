a84a605133beae8e03c11941d9a3140db452469b
Ivan Golikov <gis92@rambler.ru>
2023-02-18 11:54:06 +0300

    Testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,38 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturn100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldReturn1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldReturn0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+}
